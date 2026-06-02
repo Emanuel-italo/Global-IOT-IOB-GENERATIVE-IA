3b3ee3779c44fe04325b69a2e36587835ea3473a
Emanuel Italo <emanuelitaloleal@hotmail.com>
2026-06-02 19:14:30 -0300

    feat: definir constantes de pinos para botões, LEDs e potenciômetro no ESP32

--- main.ino
+++ main.ino
@@ -5,3 +5,10 @@
 
 const char* SSID     = "Wokwi-GUEST";
 const char* PASSWORD = "";
+
+#define BT_PRETO   18
+#define BT_BRANCO  19
+#define LED_GRE    17
+#define LED_YEL     4
+#define LED_RED     2
+#define POT        33
